7100e27f474ba1f761078600c7f9aee83eed6a58
Nick Miller <nickamilli@gmail.com>
2020-05-15 13:11:33 -0600

    Add timer and audio cues

--- assets/js/workout.js
+++ assets/js/workout.js
@@ -1,20 +1,18 @@
 /**
  * Local datastore of workouts
- * @type {[
- *  {
+ * @type {{
  *      image: string,
  *      sets: number,
  *      timer: number,
  *      description: string,
  *      title: string,
- *  }
- * ]}
+ *  }[]}
  */
 const workouts = [
     {
-        image: "",
+        image: "sp.gif",
         sets: 3,
-        timer: 30,
+        timer: 10,
         description: "Here's how it works",
         title: "Hello"
     },
@@ -27,22 +25,25 @@ const workouts = [
     },
 ];
 
-const goClick = (button, container, timer, next) => {
-    container.querySelector("#timer").textContent = `Timer: ${timer}`;
-    button.disabled = true;
-    next.disabled = false;
+const goClick = (button, container, next) => {
+    startTimer(button, container, next);
 };
 
 const initializeContainer = (container, workout) => {
     container.querySelector("h3").textContent = `Exercise: ${workout.title}`;
     container.querySelector("p").textContent = `How to do it: ${workout.description}`;
+    container.querySelector("img").src = `assets/images/${workout.image}`;
     container.querySelector("#sets").textContent = `Sets: ${currentWorkout.sets}`;
     container.querySelector("#timer").textContent = "";
 }
 
 const nextClick = (container, go, next) => {
     next.disabled = true;
+    const doneSound = container.querySelector("#timerDone");
+    doneSound.pause();
+    doneSound.currentTime = 0;
     currentWorkout.sets--;
+    currentWorkout.timer = workouts[currentIndex].timer;
     container.querySelector("#sets").textContent = `Sets: ${currentWorkout.sets}`;
     if (currentWorkout.sets < 1) {
         currentIndex++;
@@ -58,6 +59,39 @@ const nextClick = (container, go, next) => {
     go.disabled = false;
 }
 
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const startTimer = async (go, container, next) => {
+    const timer = container.querySelector("#timer");
+    const doneSound = container.querySelector("#timerDone");
+    const countdown = container.querySelector("#countdown");
+    go.disabled = true;
+    let start = 3;
+    while (start > 0) {
+        timer.textContent = `Ready in: ${start}`;
+        timer.style.color = "green";
+        countdown.play();
+        await sleep(1000);
+        start--;
+    }
+    timer.style.color = "black";
+    while (currentWorkout.timer >= 1) {
+        timer.textContent = `Timer: ${currentWorkout.timer}`;
+        if (currentWorkout.timer < 5) {
+            countdown.play();
+            timer.style.color = "red";
+        }
+        await sleep(1000);
+        currentWorkout.timer--;
+    }
+    timer.textContent = "Done!";
+    timer.style.color = "green";
+    doneSound.play();
+    next.disabled = false;
+}
+
 let currentWorkout = Object.assign({}, workouts[0]);
 let currentIndex = 0;
 
@@ -66,6 +100,6 @@ window.onload = () => {
     const go = container.querySelector("#go");
     const next = container.querySelector("#next");
     initializeContainer(container, currentWorkout);
-    go.onclick = () => goClick(go, container, currentWorkout.timer, next);
+    go.onclick = () => goClick(go, container, next);
     next.onclick = () => nextClick(container, go, next);
 }
--- workout.md
+++ workout.md
@@ -4,14 +4,24 @@ title:  "Workout"
 ---
 <script src="{{ base.url | prepend: site.url }}/assets/js/workout.js"></script>
 
-# Workout Page
-
 <div id="container">
+    <audio id="timerDone">
+        <source src="assets/audio/timerDone.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    <audio id="countdown">
+        <source src="assets/audio/countdown.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    <audio id="start">
+        <source src="assets/audio/start.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
     <h3></h3>
     <p></p>
     <span id="timer"></span>
     <span id="sets"></span>
-    <img src="assets/images/sp.gif" />
+    <img src="" />
     <button id="go">Go!</button>
     <button disabled id="next">Next set</button>
 </div>
